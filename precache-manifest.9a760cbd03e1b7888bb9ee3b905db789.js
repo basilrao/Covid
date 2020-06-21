d3549674a613142917efe3cae8a9216dce5f92d8
basilrao <basilrao123@gmai.com>
2020-06-21 08:53:35 -0700

    Updates

---
+++ precache-manifest.9a760cbd03e1b7888bb9ee3b905db789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76e0b42fa455790d01b620cc2231019",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "e1fb783344ba6a2b9de0",
+    "url": "/covid/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0202f9c3256392e74589",
+    "url": "/covid/static/css/main.53fd6395.chunk.css"
+  },
+  {
+    "revision": "e1fb783344ba6a2b9de0",
+    "url": "/covid/static/js/2.528a03a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid/static/js/2.528a03a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0202f9c3256392e74589",
+    "url": "/covid/static/js/main.0c278d43.chunk.js"
+  },
+  {
+    "revision": "2538616d296fec9cb619",
+    "url": "/covid/static/js/runtime-main.6f8ac736.js"
+  }
+]);
